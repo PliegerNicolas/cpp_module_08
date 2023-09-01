89edef72c89c33ff67beb648127fe72b02e82237
Nicolas PLIEGER <plieger05@gmail.com>
2023-09-01 17:05:26 +0200

    init ex02

---
+++ ex02/includes/MutantStack.hpp
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nplieger <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/01 17:04:05 by nplieger          #+#    #+#             */
+/*   Updated: 2023/09/01 17:05:14 by nplieger         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#pragma once
+
+#include <iostream>
+
+class	MutantStack
+{
+	private:
+		/* Attributs */
+
+		/* Constructors & Destructors */
+
+		/* Member functions */
+
+	protected:
+		/* Attributs */
+
+		/* Constructors & Destructors */
+
+		/* Member functions */
+
+	public:
+		/* Attributs */
+
+		/* Constructors & Destructors */
+		MutantStack(void);
+
+		MutantStack(const MutantStack &other);
+		MutantStack	&operator=(const MutantStack &other);
+
+		~MutantStack(void);
+
+		/* Member functions */
+};
